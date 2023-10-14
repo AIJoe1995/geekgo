90cd16e803324ca708d89d411a6c981292bdf3bb
Oasis <1980131774@qq.com>
2023-10-14 16:08:07 +0800

    homework 5

--- week5/webook/web/user_test.go
+++ week5/webook/web/user_test.go
@@ -2,98 +2,174 @@ package web
 
 import (
 	"bytes"
-	"context"
-
 	"encoding/json"
+	"errors"
 	"geekgo/week5/webook/domain"
 	"geekgo/week5/webook/service"
 	svcmocks "geekgo/week5/webook/service/mocks"
-	"geekgo/week5/webook/web/jwt"
+	ijwt "geekgo/week5/webook/web/jwt"
 	jwtmocks "geekgo/week5/webook/web/jwt/mocks"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-
 	"go.uber.org/mock/gomock"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
+type LoginSMSReq struct {
+	Phone string `json:"phone"`
+	Code  string `json:"code"`
+}
+
 func TestUserHandler_LoginSMS(t *testing.T) {
 	testCases := []struct {
-		name string
-		mock func(ctrl *gomock.Controller) (service.UserService, service.CodeService, jwt.Handler)
-
-		reqBody  string
-		biz      string
-		phone    string
-		code     string
+		name     string
+		mock     func(ctrl *gomock.Controller) (service.UserService, service.CodeService, ijwt.Handler)
+		req      LoginSMSReq
 		wantCode int
 		wantBody Result
 	}{
 		{
-			name: "验证码校验通过",
-			mock: func(ctrl *gomock.Controller) (service.UserService, service.CodeService, jwt.Handler) {
-				usersvc := svcmocks.NewMockUserService(ctrl)
+			name: "短信登录成功",
+			mock: func(ctrl *gomock.Controller) (service.UserService, service.CodeService, ijwt.Handler) {
+				svc := svcmocks.NewMockUserService(ctrl)
 				codesvc := svcmocks.NewMockCodeService(ctrl)
 				jwtHdl := jwtmocks.NewMockHandler(ctrl)
 
-				codesvc.EXPECT().Verify(gomock.Any(), "login", "15212345678", "123456").Return(true, nil)
-				usersvc.EXPECT().FindOrCreate(gomock.Any(), "15212345678").Return(domain.User{
+				codesvc.EXPECT().Verify(gomock.Any(), "login", "15012345678", "123456").Return(true, nil)
+				svc.EXPECT().FindOrCreate(gomock.Any(), "15012345678").Return(domain.User{
 					Id:    1,
-					Phone: "15212345678",
+					Phone: "15012345678",
 				}, nil)
-				jwtHdl.EXPECT().SetLoginToken(context.Background(), 1).Return(nil)
-
-				return usersvc, codesvc, jwtHdl
+				jwtHdl.EXPECT().SetLoginToken(gomock.Any(), int64(1)).Return(nil)
+				return svc, codesvc, jwtHdl
 			},
-			reqBody: `
-{
-"phone": "15212345678", 
-"code": "123456"
-}`,
-			biz:      "login",
-			phone:    "15212345678",
-			code:     "123456",
+			req: LoginSMSReq{
+				Phone: "15012345678",
+				Code:  "123456",
+			},
 			wantCode: 200,
-			wantBody: Result{Msg: "验证码校验通过"},
+			wantBody: Result{
+				Msg: "验证码校验通过",
+			},
+		},
+		{
+			name: "短信验证时出现系统错误",
+			mock: func(ctrl *gomock.Controller) (service.UserService, service.CodeService, ijwt.Handler) {
+				svc := svcmocks.NewMockUserService(ctrl)
+				codesvc := svcmocks.NewMockCodeService(ctrl)
+				jwtHdl := jwtmocks.NewMockHandler(ctrl)
+
+				codesvc.EXPECT().Verify(gomock.Any(), "login", "15012345678", "123456").Return(false, errors.New("mock codesvc error"))
+
+				return svc, codesvc, jwtHdl
+			},
+			req: LoginSMSReq{
+				Phone: "15012345678",
+				Code:  "123456",
+			},
+			wantCode: 200,
+			wantBody: Result{
+				Code: 5,
+				Msg:  "系统错误",
+			},
+		},
+		{
+			name: "短信验证失败",
+			mock: func(ctrl *gomock.Controller) (service.UserService, service.CodeService, ijwt.Handler) {
+				svc := svcmocks.NewMockUserService(ctrl)
+				codesvc := svcmocks.NewMockCodeService(ctrl)
+				jwtHdl := jwtmocks.NewMockHandler(ctrl)
+
+				codesvc.EXPECT().Verify(gomock.Any(), "login", "15012345678", "123456").Return(false, nil)
+
+				return svc, codesvc, jwtHdl
+			},
+			req: LoginSMSReq{
+				Phone: "15012345678",
+				Code:  "123456",
+			},
+			wantCode: 200,
+			wantBody: Result{
+				Code: 4,
+				Msg:  "验证码有误",
+			},
+		},
+		{
+			name: "短信登录-验证码验证通过-数据库访问失败",
+			mock: func(ctrl *gomock.Controller) (service.UserService, service.CodeService, ijwt.Handler) {
+				svc := svcmocks.NewMockUserService(ctrl)
+				codesvc := svcmocks.NewMockCodeService(ctrl)
+
+				codesvc.EXPECT().Verify(gomock.Any(), "login", "15012345678", "123456").Return(true, nil)
+				svc.EXPECT().FindOrCreate(gomock.Any(), "15012345678").Return(domain.User{}, errors.New("mock db error"))
+
+				return svc, codesvc, nil
+			},
+			req: LoginSMSReq{
+				Phone: "15012345678",
+				Code:  "123456",
+			},
+			wantCode: 200,
+			wantBody: Result{
+				Code: 5,
+				Msg:  "系统错误",
+			},
+		},
+		{
+			name: "短信登录-jwtsetlogincode失败",
+			mock: func(ctrl *gomock.Controller) (service.UserService, service.CodeService, ijwt.Handler) {
+				svc := svcmocks.NewMockUserService(ctrl)
+				codesvc := svcmocks.NewMockCodeService(ctrl)
+				jwtHdl := jwtmocks.NewMockHandler(ctrl)
+
+				codesvc.EXPECT().Verify(gomock.Any(), "login", "15012345678", "123456").Return(true, nil)
+				svc.EXPECT().FindOrCreate(gomock.Any(), "15012345678").Return(domain.User{
+					Id:    1,
+					Phone: "15012345678",
+				}, nil)
+				jwtHdl.EXPECT().SetLoginToken(gomock.Any(), int64(1)).Return(errors.New("mock jwt error"))
+				return svc, codesvc, jwtHdl
+			},
+			req: LoginSMSReq{
+				Phone: "15012345678",
+				Code:  "123456",
+			},
+			wantCode: 200,
+			wantBody: Result{
+				Code: 5,
+				Msg:  "系统错误",
+			},
 		},
 	}
 
 	for _, tc := range testCases {
-		t.Run(tc.name,
-			func(t *testing.T) {
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+			svc, codesvc, jwtHdl := tc.mock(ctrl)
+			uh := NewUserHandler(codesvc, svc, jwtHdl)
 
-				ctrl := gomock.NewController(t)
-				defer ctrl.Finish()
+			b, err := json.Marshal(tc.req)
+			require.NoError(t, err)
+			reqBody := bytes.NewBuffer(b)
+			// 构造请求
+			req, err := http.NewRequest(http.MethodPost, "/users/login_sms", reqBody)
+			req.Header.Set("Content-Type", "application/json")
+			resp := httptest.NewRecorder()
+			server := gin.Default()
+			uh.RegisterRoutes(server)
+			server.ServeHTTP(resp, req)
 
-				usersvc, codesvc, jwtHdl := tc.mock(ctrl)
+			assert.Equal(t, tc.wantCode, resp.Code)
 
-				// 测试userhandler.LoginSMS
-				userHdl := NewUserHandler(codesvc, usersvc, jwtHdl) // codeSvc svc jwtHdl 需要mock实现
+			var webRes Result
+			err = json.NewDecoder(resp.Body).Decode(&webRes)
+			require.NoError(t, err)
+			assert.Equal(t, tc.wantBody, webRes)
 
-				server := gin.Default()
-
-				userHdl.RegisterRoutes(server)
-
-				req, err := http.NewRequest(http.MethodPost,
-					"/users/login_sms", bytes.NewBuffer([]byte(tc.reqBody)))
-				require.NoError(t, err)
-				req.Header.Set("Content-Type", "application/json")
-
-				resp := httptest.NewRecorder()
-				//server.Use(func(c *gin.Context) {
-				//	c.Set("user", jwt.UserClaims{})
-				//}) // jwt怎么用？
-
-				server.ServeHTTP(resp, req)
-				assert.Equal(t, tc.wantCode, resp.Code)
-				var webRes Result
-				err = json.NewDecoder(resp.Body).Decode(&webRes)
-				require.NoError(t, err)
-				assert.Equal(t, tc.wantBody, webRes)
-
-			})
+		})
 	}
 }
